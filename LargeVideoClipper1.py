ecceccdd3cb909bd5b5b0ffece7a7bf0a0e5add6
Wave <wave_1102@hotmail.com>
2023-05-10 21:22:20 +0800

    Two ways to implement cutting a large video file.

--- LargeVideoClipper1.py
+++ LargeVideoClipper1.py
@@ -2,6 +2,11 @@ import os
 import subprocess
 from multiprocessing import Pool
 
+# 1. Obtain video information and calculate the number of segments needed, the process is the same as above.
+# 2. Define a split_video function to execute a single video splitting task.
+# 3. Use Pool to create 8 processes, and use p.map to assign n_segments video splitting tasks to 8 processes for execution.
+# 4. Wait for all processes to complete execution, then the video will be split into n_segments segments.
+
 # Change to your own large file path
 # For Mac OS: the Mobile Hard Drive(MHD) path starts with /Volumes/...
 # For Windows OS: the MHD path start with drive letter "D:/"" or "E:/" ...
@@ -23,6 +28,7 @@ def split_video(i):
     command = ['ffmpeg', '-i', VIDEO_PATH, '-ss', str(start_time), '-t', str(end_time - start_time), 
                '-c', 'copy', output_file]
     subprocess.run(command)
+
 
 if __name__ == '__main__':
     //You can change the concurrent process number. 3-8 recommended.
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # LargeVideoClipper
 Large Video Clipper
+
+Method 1：
+LargeVideoClipper1.py
+
+```Install ffmpeg and ffprobe. You can follow these steps:
+- Download ffmpeg: You can download it from https://ffmpeg.org/download.html, choose the Mac OSX version
+- Unzip the downloaded file, and run in the terminal:
+bash
+sudo mv ffmpeg /usr/local/bin 
+sudo mv ffprobe /usr/local/bin
+- Then you can call ffmpeg and ffprobe in Python. 
+```
+I have experimented with cutting a 2G video file, which took 2-3 minutes.
+
+
+方法一：
+LargeVideoClipper1.py
+```
+安装ffmpeg和ffprobe。可以按照以下步骤进行:
+- 下载ffmpeg:可以从https://ffmpeg.org/download.html下载,选择Mac OSX版本
+- 解压下载的文件,在终端运行:
+bash
+sudo mv ffmpeg /usr/local/bin
+sudo mv ffprobe /usr/local/bin
+- 然后就可以在Python中调用ffmpeg和ffprobe了
+```
+实验了下切割2G左右的视频文件，用时2-3分钟。
